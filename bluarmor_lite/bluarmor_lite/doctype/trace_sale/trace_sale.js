c78d221957e9f5dbb46bec46b35298473760d6df
alwinmartis <alwinmartis20@gmail.com>
2024-12-10 16:31:23 +0530

    chore: Trace Sale doctype

---
+++ bluarmor_lite/bluarmor_lite/doctype/trace_sale/trace_sale.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, alwin martis and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Trace Sale", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ bluarmor_lite/bluarmor_lite/doctype/trace_sale/trace_sale.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, alwin martis and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TraceSale(Document):
+	pass
