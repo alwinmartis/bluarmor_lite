a001098cd85e09ceed4e223d22a14d2a9ad5fdec
alwinmartis <alwinmartis20@gmail.com>
2024-12-11 16:42:01 +0530

    feat: data collection from app to dashboard

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,3 +1,19 @@
 <template>
-    from Dashboard.vue
+    <div>
+        Welcome to Bluarmor App!
+    </div>
   </template>
+
+  <script setup>
+  import { createListResource } from "frappe-ui";
+//   import {sessionUser} from "../data/session";
+
+  const salestarger = createListResource({
+    doctype: "Trace Sale",
+    fields:["target_quantity","from_date","to_date"],
+    auto:true,
+    onSuccess(d){
+       console.log(d);
+  }
+  })
+</script>
