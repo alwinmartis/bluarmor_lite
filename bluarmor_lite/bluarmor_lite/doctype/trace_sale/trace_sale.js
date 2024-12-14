c13e2190c42e9fe5db29bdad1632ccb69b51d4a5
alwinmartis <alwinmartis@gmail.com>
2024-12-14 18:10:44 +0530

    feat: backend sales data collection added

--- bluarmor_lite/bluarmor_lite/doctype/trace_sale/trace_sale.js
+++ bluarmor_lite/bluarmor_lite/doctype/trace_sale/trace_sale.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2024, alwin martis and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Trace Sale", {
-// 	refresh(frm) {
+frappe.ui.form.on("Trace Sale", {
+	refresh:function(frm){
+        console.log("In")
+        if(frm.doc.from_date && frm.doc.to_date && frm.doc.item){
+            console.log("inside")
+            frappe.call({
+                method:"bluarmor_lite.bluarmor_lite.doctype.trace_sale.trace_sale.calculate_qty",
+                args:{
+                    doc_name:frm.doc.name
+                },
+                callback:function(r){
+                    console.log(r)
+                    if(r.message){
+                        frm.set_value("completed_quantity",r.message.completed_qty);
+                        frm.set_value("pending_quantity",r.message.pending_qty);
+                        console.log(r.item)
+                    }
+                }
+            })
+        }
+    }
 
-// 	},
-// });
+})
--- bluarmor_lite/bluarmor_lite/doctype/trace_sale/trace_sale.py
+++ bluarmor_lite/bluarmor_lite/doctype/trace_sale/trace_sale.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2024, alwin martis and contributors
 # For license information, please see license.txt
 
-import frappe
-from frappe.model.document import Document
-
+import frappe # type: ignore
+from frappe.model.document import Document # type: ignore
+from frappe.utils import flt # type: ignore
 
 class TraceSale(Document):
 	# begin: auto-generated types
@@ -12,13 +12,46 @@ class TraceSale(Document):
 	from typing import TYPE_CHECKING
 
 	if TYPE_CHECKING:
-		from frappe.types import DF
+		from frappe.types import DF # type: ignore
 
 		completed_quantity: DF.Int
 		from_date: DF.Date | None
+		item: DF.Link | None
+		pending_quantity: DF.Int
 		target_quantity: DF.Int
 		to_date: DF.Date | None
 	# end: auto-generated types
 
-	def validate(self):
-		pass
+	# def onload(self):
+	# 	self.pending_quantity = self.target_quantity - self.completed_quantity
+	# 	print(self.pending_quantity)
+
+	def calculate_qty(self):
+		print("server")
+		if not self.from_date or not self.to_date or not self.item:
+			frappe.throw("Please validate the data!")
+
+		completed_qty = frappe.db.sql("""
+				SELECT sum(`dni`.qty) AS `qty`
+					FROM `tabDelivery Note Item` As `dni`
+					Inner Join `tabDelivery Note` As `dn`
+						On `dni`.parent = `dn`.name
+					WHERE `dni`.item_code = %s
+					AND `dn`.posting_date Between %s AND %s
+			""",(self.item, self.from_date, self.to_date))[0][0] or 0
+		
+		self.completed_quantity=completed_qty
+		self.pending_quantity = self.target_quantity - completed_qty
+
+		self.save()
+		return{
+			"completed_qty":self.completed_quantity,
+			"pending_qty":self.pending_quantity,
+			"item":self.item
+		}
+@frappe.whitelist()
+def calculate_qty(doc_name):
+	print("Hello")
+	doc = frappe.get_doc("Trace Sale",doc_name)
+	return doc.calculate_qty()
+
