34b0e285fac13075fd1430f5662ef6f195e59b00
alwinmartis <alwinmartis@gmail.com>
2024-12-17 13:58:20 +0530

    feat: Data collection done

--- frontend/src/pages/Newtarget.vue
+++ frontend/src/pages/Newtarget.vue
@@ -1,37 +1,67 @@
 <template>
     <div class="head">
         <p class="head-name">Create Your Targets</p>
+        <ErrorMessage :message="salesListResource.insert.error" />
     </div>
     <div class="body-data">
+        
         <div class="body-data-in">
-            <p class="name">From Date</p>
-            <FormControl placeholder="From Date" type="date"></FormControl>
+            <p class="name" >From Date</p>
+            <FormControl v-model="targetDetails.fdate" placeholder="From Date" type="date" lable="fdate"></FormControl>
         </div>
         <div class="body-data-in">
             <p class="name">To Date</p>
-            <FormControl placeholder="To Date" type="date"></FormControl>
+            <FormControl v-model="targetDetails.tdate" placeholder="To Date" type="date" lable="tdate"></FormControl>
         </div> 
     </div>
     <div class="body-data">
         <div class="body-data-in">
             <p class="name">Item name</p>
-            <FormControl placeholder="Item" type="taxt"></FormControl>
+            <FormControl v-model="targetDetails.item" placeholder="Item" type="taxt" lable="item"></FormControl>
         </div>
         <div class="body-data-in">
             <p class="name">Target Quantity</p>
-            <FormControl placeholder="Quantity" type="text"></FormControl>
+            <FormControl v-model="targetDetails.tqty" placeholder="Quantity" type="text" lable="tqty"></FormControl>
         </div> 
     </div>
     <div class="body-data">
     <div class="save">
-        <Button class="button">Save </Button>
+        <Button :loading="salesListResource.insert.loading" class="button" @click="savetarget">Save </Button>
     </div>
 </div>
     
 </template>
+
+
 <script setup>
-import { FormControl, Button } from 'frappe-ui';
+import { FormControl, Button, Dropdown, Autocomplete, createListResource, createResource, ErrorMessage } from 'frappe-ui';
+import {reactive, ref, computed, inject} from "vue";
+
+const targetDetails = reactive({
+    fdate: null,
+    tdate: null,
+    item: '',
+    tqty: 0.0,
+})
+
+const salesListResource = createListResource({
+    doctype:"Trace Sale",
+
+})
+
+
+function savetarget(){
+    salesListResource.insert.submit({
+        ...targetDetails,
+    from_date:targetDetails.fdate,
+to_date:targetDetails.tdate,
+item:targetDetails.item,
+target_quantity:targetDetails.tqty
+})
+}
+
 </script>
+
 
 <style scoped>
 .body{
@@ -39,7 +69,9 @@ import { FormControl, Button } from 'frappe-ui';
 }
 .head{
     display:flex;
+    flex-direction: column;
     justify-content: center;
+    align-items: center;
     margin:25px 10px;
     background-color:azure;
 }
